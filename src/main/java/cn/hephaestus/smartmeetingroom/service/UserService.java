1bec29c47b972edc81751e65c7bc6ab608f369d7
sunwang1 <995700257@qq.com>
2018-12-03 15:11:46 +0800

    增加了userInfo实体类，增加了userinfo的接口。

--- src/main/java/cn/hephaestus/smartmeetingroom/service/UserService.java
+++ src/main/java/cn/hephaestus/smartmeetingroom/service/UserService.java
@@ -1,6 +1,7 @@
 package cn.hephaestus.smartmeetingroom.service;
 
 import cn.hephaestus.smartmeetingroom.model.User;
+import cn.hephaestus.smartmeetingroom.model.UserInfo;
 
 public interface UserService {
     public Boolean login(String userName,String password);
@@ -8,4 +9,10 @@ public interface UserService {
     public User findUserByUserName(String userName);
 
     public void register(User user);
+
+    public boolean addUserinfo(Integer id,UserInfo userInfo);
+
+    public boolean alterUserInfo(Integer id,UserInfo userInfo);
+
+    public UserInfo getUserInfo(Integer id);
 }
